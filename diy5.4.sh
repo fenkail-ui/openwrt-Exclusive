1b9b4c2b3d95d0852eff7794a31ed4f46ca67fcb
fenkail-ui <64298897+fenkail-ui@users.noreply.github.com>
2021-08-22 10:54:12 +0800

    Update diy5.4.sh

--- diy5.4.sh
+++ diy5.4.sh
@@ -33,3 +33,6 @@ git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-ser
 # git clone https://github.com/project-lede/luci-app-godproxy.git package/luci-app-godproxy
 # git clone https://github.com/garypang13/luci-app-dnsfilter.git package/luci-app-dnsfilter
 # git clone https://github.com/ITdesk01/jd_openwrt_script.git package/jd_openwrt_script
+git clone https://github.com/fenkail-ui/x86.git
+\cp -rf x86 ./target/linux/
+rm -rf x86
